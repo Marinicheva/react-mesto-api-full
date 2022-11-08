7c3ecfa92c22308b3a4a98d8333ab5ca24b1adce
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-08 18:31:49 +0300

    feat: send email when user login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -60,7 +60,7 @@ const login = (req, res, next) => {
           maxAge: 3600000,
           httpOnly: true,
         })
-        .send({ message: 'Авторизация прошла успешно' });
+        .send({ message: 'Авторизация прошла успешно', email: user.email });
     })
     .catch((err) => {
       next(err);
