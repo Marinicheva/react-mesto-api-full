71907023c810f296b312bb67c3b104f5e7bf69b0
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-13 18:23:21 +0300

    refactor: usage sign key for token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
